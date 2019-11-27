c9a1efca1752d56fe497dcb4df3630b1c4b5595f
Shay Naimark <sn1239@gmail.com>
2019-11-27 09:54:29 +0200

    credentials - home page and test page

--- HomePage.js
+++ HomePage.js
@@ -3,7 +3,7 @@ const Login = require("./Login")
 class HomePage {
     constructor(selenium, userName, password) {
         this.selenium = selenium;
-        this.login = new Login(selenium, userName, password);
+        this.login = new Login(this.selenium, userName, password);
     }
 }
 
--- Test.js
+++ Test.js
@@ -2,14 +2,14 @@ const HomePage = require("./HomePage");
 const BasePage = require("./BasePage");
 
 class Test {
-    constructor() {
+    constructor(userName, password) {
         this.testSelenium = new BasePage().selenium;
-        this.homePage = new HomePage(this.testSelenium, "Bootuser01", "Boot1234");
+        this.homePage = new HomePage(this.testSelenium, userName, password);
     }
     async login() {
         await test1.homePage.login.navigateToLoginPage();
         await test1.homePage.login.login();
     }
 }
-let test1 = new Test();
+let test1 = new Test("Bootuser01", "Boot1234");
 test1.login()
