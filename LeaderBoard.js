51649e29278c0a71e1b459a89f583d94083c7dde
Eldad cohen <eldadco669@gmail.com>
2019-12-02 12:37:09 +0200

    leader board class

---
+++ LeaderBoard.js
@@ -0,0 +1,24 @@
+class LeaderBoard {
+
+    constructor(selenium) {
+        this.selenium = selenium
+        this.rank = null
+
+    }
+
+  async getRank() {
+
+        let rank = await this.selenium.findElementBy('xpath', '//*[@class="widget-left ng-star-inserted"]//*[@class="value-out-of-max ng-star-inserted"]//*[@class="big ng-star-inserted"][1]')
+        let textRank = await this.selenium.getTextFromElement(null, null, rank)
+        let fromRank = await this.selenium.findElementBy('xpath', '//*[@class="widget-left ng-star-inserted"]//*[@class="value-out-of-max ng-star-inserted"]//*[@class="big ng-star-inserted"][2]')
+        let textFromRank = await this.selenium.getTextFromElement(null, null, fromRank)
+        this.rank = textRank + textFromRank
+        return this.rank
+    }
+    async click() {
+
+        await this.selenium.clickButton('className', 'leaderboard-widget widget ng-star-inserted')
+        await this.selenium.URLvalidation('leaderboard')
+    }
+}
+module.exports = LeaderBoard
