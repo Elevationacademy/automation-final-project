2e9f4c6c32d836c3a452abe833f045550b75cb87
RebaccaNahom <rln770@gmail.com>
2019-12-03 13:54:02 +0200

    test for leaderBoardClass file 2nd version

--- test.js
+++ test.js
@@ -1,46 +1,33 @@
-const BasePage = require("./BasePage");
-const code = require("./code")
-const login = require('./Login')
-const home = require('./HomePage')
-// const logger = require('./winstone');
+const BasePage = require("./basePage")
+const HomePage = require("./HomePage");
+const LeaderBoard = require("./leaderBoardClass")
 
-class ClientsPageTest {
+class LeaderBoardTest {
     constructor() {
-        this.bp = new BasePage().selenium
-        this.logIn = new login(this.bp)
-        this.page = new code(this.bp)
-        this.hp = new home(this.bp,'Bootuser05', 'Boot1234')
+        this.basePage = new BasePage()
+        this.testSelenium = this.basePage.selenium
+        this.logger = this.basePage.logger
+        this.leaderBoard = new LeaderBoard(this.testSelenium, this.logger)
+        this.homePage = new HomePage(this.testSelenium, "Bootuser22", "Boot1234");
 
-        }
-      
+    }
+    async infoUserTest() {
+        await this.homePage.login.navigateToLoginPage()
+        await this.homePage.login.login()
+        await this.leaderBoard.navigate()
+        await this.leaderBoard.usersInfo()
+        setTimeout(async () => {
 
-        async oneUserInfo(){
-            await this.hp.login.navigateToLoginPage()
-            await this.hp.login.login()
-            setTimeout(() => {
-            console.log("count to 10, it's timer")
-             this.page.getInfoAboutUser("0")},10000)
-        }
+        await this.leaderBoard.allUsersArr[0].getRank()
+        await this.leaderBoard.allUsersArr[0].getName()
+        await this.leaderBoard.allUsersArr[0].getTeam()
+        await this.leaderBoard.allUsersArr[0].getLevel()
+        await this.leaderBoard.allUsersArr[0].getScore()
 
+        }, 15000)
 
-
-        async click(){
-            await this.hp.login.navigateToLoginPage()
-            await this.hp.login.login()
-            setTimeout(() => {
-                console.log("count to 10, it's timer")
-            this.page.clickUser('0')},10000)
-        }
-
-        async allUsersInfo(){
-            await this.hp.login.navigateToLoginPage()
-            await this.hp.login.login()
-            setTimeout(() => {
-            this.page.getInfoAboutUsers() 
-        },10000)
-        }
+    }
 }
-let xx = new ClientsPageTest()
-// xx.oneUserInfo()
-xx.click()
-// xx.allUsersInfo()
+
+let leaderBoard = new LeaderBoardTest()
+leaderBoard.infoUserTest()
